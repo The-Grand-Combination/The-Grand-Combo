fa70c194593ca75a0f6c298ac8f1976d9b182b95
emprahtheodosius <emprah.theodosius@gmail.com>
2023-04-15 22:35:57 +0200

    .

--- TGC/common/defines.lua
+++ TGC/common/defines.lua
@@ -661,7 +661,7 @@ defines = {
 
 		POP_SAVINGS = 0.018,
 
-		STATE_CREATION_ADMIN_LIMIT = 0.015,
+		STATE_CREATION_ADMIN_LIMIT = 0.012,
 		MIL_TO_JOIN_REBEL = 8, -- Rebels over this will join a faction
 		MIL_TO_JOIN_RISING = 9, -- Rebels over this will join a general rising
 		MIL_TO_AUTORISE = 9, -- Rebels over this rise no matter what
